548909acb7cb16e6c023620ce17a0c54e58524ef
Николай Кузичкин <gora60@ya.ru>
2026-07-29 20:32:31 +0300

    feat: move ConnectionView onto Zafiro layout containers, drop label converter

--- ArctZ/Converters/ConnectionStateConverters.cs
+++ ArctZ/Converters/ConnectionStateConverters.cs
@@ -8,24 +8,6 @@ using System.Globalization;
 
 namespace ArctZ.Converters;
 
-public class ConnectionStateToLabelConverter : IValueConverter
-{
-    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
-    {
-        return value switch
-        {
-            ConnectionState.Disconnected => "Не подключено",
-            ConnectionState.Connecting => "Подключение…",
-            ConnectionState.Connected => "Подключено",
-            ConnectionState.Reconnecting => "Переподключение…",
-            _ => "—",
-        };
-    }
-
-    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
-        => throw new NotSupportedException();
-}
-
 public class ConnectionStateToBrushConverter : IValueConverter
 {
     public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
