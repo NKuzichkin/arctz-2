cb31d37d5274c624e898df0f7f756ae6e3c54463
Николай Кузичкин <gora60@ya.ru>
2026-08-16 07:50:46 +0300

    feat: enumerate paired Bluetooth COM ports with real names on Desktop

--- ArctZ.Desktop/Program.cs
+++ ArctZ.Desktop/Program.cs
@@ -21,6 +21,7 @@ namespace ArctZ.Desktop
             var services = new ServiceCollection();
             services.AddArctZCore();
             services.AddSingleton<IDeviceTransport, DesktopSerialTransport>();
+            services.AddSingleton<IDeviceEndpointProvider, DesktopBluetoothEndpointProvider>();
             services.AddSingleton<IProgramStorage>(_ => new JsonFileProgramStorage(
                 Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "ArctZ", "Programs")));
             App.Services = services.BuildServiceProvider();
